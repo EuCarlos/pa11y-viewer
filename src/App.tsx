3af799417c62659802bfaff5be0e46740c73a563
Carlos Alves <carlosalves369m@hotmail.com>
2022-11-14 18:38:58 -0300

    :lipstick: adds number of reports and stamp written Created by Carlos Alves

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { UID } from './utils/uid'
 import './styles/globals.sass'
 
 function App() {
+  const numberOfReports = json.length
+
   const reports = json.map(({ code, type, typeCode, message, context, selector }) => {
     return <div>
       <Report 
@@ -15,12 +17,15 @@ function App() {
         context={context}
         selector={selector}
       />
-    </div>
+    </div>    
   })
 
   return (
     <div className="App">
-      {reports}
+      <h2>📝 {numberOfReports} Reports</h2>
+      {numberOfReports <= 0 ? <div className={'report--not-found'}>No reports found</div>: reports}
+
+      <footer>Created by <a href="https://github.com/EuCarlos/pa11y-viewer">Carlos Alves</a></footer>
     </div>
   )
 }
