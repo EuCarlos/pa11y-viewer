1450e530bdee0f804b4baaa8d40901c5727af93c
Carlos Alves <carlosalves369m@hotmail.com>
2022-11-21 18:23:59 -0300

    :heavy_plus_sign: add prismjs

--- src/utils/messages.ts
+++
@@ -1,5 +0,0 @@
-export const typesCodeMessage = [
-    "Pa11y ran successfully, and there are no errors",
-    "Pa11y failed run due to a technical fault",
-    "Pa11y ran successfully but there are errors in the page"
-]
